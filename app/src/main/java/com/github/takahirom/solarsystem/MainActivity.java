bfca68c4691da5236404e7b584716242032c3b90
takahirom <takam.dev@gmail.com>
2016-03-26 21:10:07 +0900

    Add licenses.

--- app/src/main/java/com/github/takahirom/solarsystem/MainActivity.java
+++ app/src/main/java/com/github/takahirom/solarsystem/MainActivity.java
@@ -5,9 +5,18 @@ import android.content.Intent;
 import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.licenses.License;
+import de.psdev.licensesdialog.licenses.MITLicense;
+import de.psdev.licensesdialog.model.Notice;
+import de.psdev.licensesdialog.model.Notices;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,4 +40,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_license) {
+            final Notices notices = new Notices();
+            notices.addNotice(new Notice("LicensesDialog", "http://psdev.de", "Copyright 2013 Philip Schiffer <admin@psdev.de>", new ApacheSoftwareLicense20()));
+
+            notices.addNotice(new Notice("MikeOrtiz/TouchImageView", "https://github.com/MikeOrtiz/TouchImageView", "Copyright (c) 2012 Michael Ortiz", new MITLicense()));
+            new LicensesDialog.Builder(this)
+                    .setNotices(notices)
+                    .build()
+                    .show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
